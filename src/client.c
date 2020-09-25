e207b4fa76a6b4fa8b949db4e8004f377aa251b8
Stephen Cochrane <skippycochrane@gmail.com>
2020-09-26 00:23:42 +0200

    Slight refactor

--- src/client.c
+++ src/client.c
@@ -12,8 +12,8 @@ static GtkWidget *res;
 */
 
 static GtkWidget *fixed;
-static GtkWidget *send;              /* Send text button */
-static GtkWidget *recv;              /* Display recv data */
+static GtkWidget *send_btn;          /* Send text button */
+static GtkWidget *recv_box;          /* Display recv data */
 static GtkWidget *text;              /* Input text */
 static GtkWidget *name;              /* Name of the program */
 static GtkWidget *voip_joined;       /* List of people joined */
@@ -43,8 +43,8 @@ main(int argc, char *argv[])
 
 	/* Map the GTK Widgets from the builder to thier Counterparts */
 	fixed = GTK_WIDGET(gtk_builder_get_object(builder, "fixed"));
-	send = GTK_WIDGET(gtk_builder_get_object(builder, "send"));
-	recv = GTK_WIDGET(gtk_builder_get_object(builder, "recv"));
+	send_btn = GTK_WIDGET(gtk_builder_get_object(builder, "send"));
+	recv_box = GTK_WIDGET(gtk_builder_get_object(builder, "recv"));
 	text = GTK_WIDGET(gtk_builder_get_object(builder, "text"));
 	name = GTK_WIDGET(gtk_builder_get_object(builder, "name"));
 	voip_joined = GTK_WIDGET(gtk_builder_get_object(builder, "voip_joined"));
@@ -53,7 +53,7 @@ main(int argc, char *argv[])
 	user_online = GTK_WIDGET(gtk_builder_get_object(builder, "user_online"));
 
 	/* Map the buttons to thier handlers */
-	g_signal_connect(send, "clicked", G_CALLBACK(on_send_clicked), NULL);
+	g_signal_connect(send_btn, "clicked", G_CALLBACK(on_send_clicked), NULL);
 
 	gtk_widget_show(window);
 	gtk_main();
--- src/client.h
+++ src/client.h
@@ -5,5 +5,15 @@
 
 #include <gtk-3.0/gtk/gtk.h>
 #include <gtk-3.0/gtk/gtkx.h>
+#include <unistd.h> 
+#include <pthread.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+
+/* Standerd Libraries*/
 #include <stdlib.h>
 #include <stdio.h>
+
+void soc_read(char *buff);
